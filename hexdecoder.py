56a66d6947119e81c2a4580a4db7ff90ae0cf4b9
morosow <53479147+morosow@users.noreply.github.com>
2020-04-19 15:59:29 +0300

    Add files via upload

---
+++ hexdecoder.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import codecs
+
+input_filename = 'input.csv'
+output_filename = 'output.csv'
+
+df = pd.read_csv(input_filename, sep = ';', skip_blank_lines=True, error_bad_lines=False)
+
+df.index = range(0, len(df.index))
+
+# In this case values (to decoding) are contained in 'description' column
+
+# Deleting '0x' from strings begin
+df.description = df.description.apply(lambda x: x[2:])
+
+# Converting hex to utf
+df.description = df.description.apply(lambda x: codecs.decode(x, "hex").decode('utf-8'))
+
+df.to_csv(output_filename, header=True, sep=';', index=False)
